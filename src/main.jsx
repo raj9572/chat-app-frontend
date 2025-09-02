827704d9dc8c2b5d4a58072edb192accee11bbb0
rajali <rajali1432@gmail.com>
2025-09-02 15:49:57 +0530

    integrate redux presist

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import { Toaster } from "react-hot-toast";
 import { Provider } from 'react-redux'
-import store from './redux/store.js';
+import store, { persistor } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    <Toaster />
-  </StrictMode>,
+  <>
+     <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+            <Toaster />
+          </PersistGate>
+        </Provider>
+    
+  </>,
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,49 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from "./userSlice.js"
 import messageReducer from "./messageSlice.js"
 import socketReducer from './socketSlice.js'
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    message: messageReducer,
-    socket: socketReducer
-  },
-})
+import storage from 'redux-persist/lib/storage';
+import {
+      persistReducer,
+      persistStore,
+      FLUSH,
+      REHYDRATE,
+      PAUSE,
+      PERSIST,
+      PURGE,
+      REGISTER,
+    } from 'redux-persist';
 
+
+const persistConfig = {
+  key: 'root', // The key for the storage in localStorage
+  version: 1,
+  storage, // The storage engine to use (e.g., localStorage, sessionStorage)
+  whitelist: ['user', 'message'], // Optional: only persist specific parts of the state
+  // blacklist: ['ui'], // Optional: don't persist specific parts of the state
+};
+
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  message: messageReducer,
+  socket: socketReducer
+  // Add other slices here
+});
+
+ const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store =  configureStore({
+      reducer: persistedReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
+    });
+    
+
+export const persistor = persistStore(store);
 
 export default store
