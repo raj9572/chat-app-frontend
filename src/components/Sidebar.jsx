8c5610b1e0acdb2cf04c111f213e8c21d514dcff
rajali <rajali1432@gmail.com>
2025-09-09 01:15:28 +0530

    implement search user feature complete

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useCallback, useEffect, useState } from 'react'
 import { LogOut, Search } from 'lucide-react';
 import OtherUsers from './OtherUsers';
 import axios from 'axios';
 import toast from 'react-hot-toast'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
-import {    setAuthUser,  setOtherUsers } from '../redux/userSlice';
+import { setAuthUser, setOtherUsers } from '../redux/userSlice';
 import { disconnectSocket } from '../socket';
+import debounce from 'lodash/debounce';
 
 
 const Sidebar = () => {
     const navigate = useNavigate()
     const [search, setSearch] = useState("")
     const dispatch = useDispatch()
-    const { otherUsers} = useSelector(store => store.user)
-    
-    const LogoutHandler = async() =>{
+    const { otherUsers } = useSelector(store => store.user)
+
+    const LogoutHandler = async () => {
         try {
             const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/user/logout`)
             toast.success(res?.data?.message)
@@ -23,22 +25,39 @@ const Sidebar = () => {
             dispatch(setAuthUser(null))
             disconnectSocket();
         } catch (error) {
-             toast.error(error.response.data.message);
+            toast.error(error.response.data.message);
         }
     }
 
     //Todo optimized search submit handler
 
-  const searchSubmitHandler = (e) =>{
-    e.preventDefault()
-    const conversationUser = otherUsers?.filter(user => user?.fullName.toLowerCase().includes(search.toLowerCase()))
-    // console.log(conversationUser)
-    if (conversationUser) {
-        dispatch(setOtherUsers(conversationUser))
-    } else {
-        toast.error("user not found")
+    const searchSubmitHandler = async(query) => {
+        // console.log(query)
+        
+        const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/v1/user?name=${query}`)
+        
+        dispatch(setOtherUsers(res?.data))
+       
     }
-  }
+
+    const debouncedFetchResults = useCallback(
+        debounce((query) => {
+            searchSubmitHandler(query);
+        }, 1000),
+        []// Ensure the debounce function is created only once
+    );
+
+    useEffect(() => {
+
+        debouncedFetchResults(search)
+
+        return () => {
+            debouncedFetchResults.cancel();
+        };
+    }, [debouncedFetchResults, search]);
+
+
+
 
 
 
@@ -46,7 +65,7 @@ const Sidebar = () => {
         <div className="w-80 bg-gray-800 border-r border-gray-700 flex flex-col shadow-lg">
             {/* search bar */}
             <div className="p-4">
-                <form onSubmit={searchSubmitHandler} className="relative">
+                <div className="relative">
                     <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
                     <input
                         value={search}
@@ -55,7 +74,7 @@ const Sidebar = () => {
                         placeholder="Search..."
                         className="w-full bg-gray-700 text-white pl-10 pr-4 py-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
                     />
-                </form>
+                </div>
             </div>
 
             <OtherUsers />
@@ -65,8 +84,8 @@ const Sidebar = () => {
             <div className="p-4 border-t border-gray-700">
                 <button className="flex items-center text-gray-300  w-full ">
                     <div onClick={LogoutHandler} className='flex items-center gap-0 cursor-pointer hover:text-white transition-colors' >
-                    <LogOut className="w-5 h-5 mr-2 " />
-                      Logout
+                        <LogOut className="w-5 h-5 mr-2 " />
+                        Logout
                     </div>
                 </button>
             </div>
